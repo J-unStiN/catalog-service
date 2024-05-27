b0b02ff933ade38abd3f02fac741c28909e8059b
Park Sung Jun <junstin119@gmail.com>
2024-05-28 08:00:31 +0900

    [feat] 단위테스트 설정 - MockBean 으로 가짜객체 주입

---
+++ src/test/kotlin/com/ktspring/catalogservice/service/GreetingServiceTest.kt
@@ -0,0 +1,40 @@
+package com.ktspring.catalogservice.service
+
+import com.ktspring.catalogservice.controller.GreetingController
+import io.mockk.every
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.reactive.server.WebTestClient
+
+
+@WebMvcTest(controllers = [GreetingController::class])
+@AutoConfigureWebTestClient
+class GreetingServiceTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockBean
+    lateinit var greetingService: GreetingService
+
+
+    @Test
+    fun getGreeting() {
+
+        val name = "sj";
+
+        val returnResult = webTestClient.get()
+            .uri("/v1/greetings/{name}", name)
+            .exchange()
+            .expectStatus().is2xxSuccessful
+            .expectBody(String::class.java)
+            .returnResult()
+
+        assertEquals("Hello, $name! Hello profile!", returnResult.responseBody)
+    }
+
+}
